d01a0b7f5fa3c9c25878d34a1c8d7d9c0596ef79
Mariano Renna <mariano1177@hotmail.com>
2023-11-22 17:22:39 -0300

    algoritmo sumador pares impares

---
+++ Algoritmo/Algoritmo.py
@@ -0,0 +1,20 @@
+import random
+
+numero = random.randint(10000000,9999999999)
+def sumador_pares(numero):
+
+    numstrings = str(numero)    #convierto a string el numero para recorrerlo
+    numfinal=0                  #variable para guardar el total
+    for i in numstrings:
+        print("valor en modo string de i", i)
+        if int(i)%2 ==0:        #convierto a int la variable string y verifico si es multiplo divisible por 2, par
+            print("valor de i convertido en int que si es par lo suma",i)
+            numfinal=numfinal+int(i)   # se suma si es par
+        else:
+            print("numero negativo: ",int(i))
+            numfinal=numfinal+int(i)-3  # si es impar le resta 3 a la variable y lo suma
+    print("el numero total es ",numfinal)    
+
+print("elnumero random sera: ",numero)
+sumador_pares(numero)
+    
