98c4778f2be91b463d997869009025dce8153588
Mariano Renna <mariano1177@hotmail.com>
2023-11-23 18:13:12 -0300

    archivos

--- Archivos/archivos2.py
+++ Archivos/archivos2.py
@@ -1,8 +1,31 @@
+import csv
 
 
+with open('archivo83.csv', 'r') as archivo:
+    lector = csv.DictReader(archivo)
+    
+    notas =0
+    
+    for fila in lector:
+        
+        dato = (fila)
+        print (f"{dato}" )
+        
+archivo.close()
 
-with open("archivo.csv", "a") as archivo:
+notas = [
+    ['Lucas', 'Burki', 7],
+    ['Marcos', 'Castelo', 5],
+    ['Luis', 'Renna', 4],
+    ['Micaela', 'Vidal', 9],
+    ['Agustina', 'Ruiz', 3],
+    ['Daniel', 'Lopez', 8],
+    ['Josefina', 'Gonzalez', 2]
+        ]
 
-
-
-    archivo.close( )
+#with open('archivo83.csv', 'w') as archivo1:
+#    escritor = csv.writer(archivo1)
+#    for fila in notas:
+#        escritor.writerow(fila)
+#
+#archivo1.close()
