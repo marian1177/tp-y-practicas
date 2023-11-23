1b58aefa1d3227f8f0591a6da125d1d6b6eaa821
Mariano Renna <mariano1177@hotmail.com>
2023-11-23 00:32:09 -0300

    rec

--- Recursividad/recursividad.py
+++ Recursividad/recursividad.py
@@ -10,12 +10,11 @@ def primos(numero):
    for n in range(1, numero + 1):  #doble iteracion para tomar el rango del numero y el rango de divisores
        for d in range(1, n + 1):
            if n % d == 0:  # si el resto modulo de la division es cero encontro divisor
-               cont+=1
-               
+               cont+=1         
        if cont == 2:  # si el contador es 2 quiere decir que solo se divide por 1 y por si mismo
            print (f"el numero {n} es primo y se suma")
            total= total+n
-       cont = 0 #se reinicia el contador para el siguiente siclo
+       cont = 0 #se reinicia el contador para el siguiente ciclo
    print ("total",total)
 
 
